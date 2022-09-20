37e153f64f9d63413150e55ee3c131ae25ff5671
LOWERCASE <lowercase.inbox@gmail.com>
2022-09-20 15:30:25 -0400

    bot working!

--- ImageClicker/Main.py
+++ ImageClicker/Main.py
@@ -1,8 +1,11 @@
 # target naming scheme: `[x offset] [y offset] [image name].png`
-# example target: `0 0 start.png`
+# example: `-20 50 start.png` will click 20 pixels to the left and
+# 50 pixels beneath the center of start.png when it is found onscreen.
 
-# this bot scans its working directory for properly named png files as targets.
+# this bot scans its working directory for png files as targets.
 # when it finds a target onscreen, it will click at it, with respect to offset.
+# if no offset is found, it will click at its center.
+# note: the bot is colourblind.
 
 # offset is in pixels; x from left to right, and y from up to down.
 # both are relative to the center of each target's screenshot.
@@ -10,39 +13,53 @@
 # pressing escape terminates the bot.
 
 
-import pyautogui
-import time
-from pynput.keyboard import Key, Listener
-import threading
-import cv2
+from pynput.mouse import Button
+from pynput.mouse import Controller as MouseController
+from pynput.keyboard import Key
+from pynput.keyboard import Listener as KeyListener
 from PIL import ImageGrab
+import cv2
+import time
+import threading
 import numpy
 import os
 import sys
 
-# increasing y lowers click point
-def click_target(frame, target, offset = (0, 0), threshold=0.8):
-	point = cv2.matchTemplate(frame, target, cv2.TM_CCOEFF_NORMED)
-	point = numpy.where(point > threshold)
-	if point[0] and point[1]:
+mouse = MouseController()
+
+def click_target(frame, target, offset=(0, 0), threshold=0.8):
+	result = cv2.matchTemplate(frame, target, cv2.TM_SQDIFF_NORMED)
+	min_value, max_value, min_point, max_point = cv2.minMaxLoc(result)
+	point = (numpy.array([min_point[1],]), numpy.array([min_point[0],]))
+	# print(min_value)
+	if min_value < 1 - threshold and point[0].size > 0 and point[1].size > 0:
 		point = point[1][0] + target.shape[1] / 2, point[0][0] + target.shape[0] / 2
 		point = (point[0] + offset[0], point[1] + offset[1])
 		human_click(point)
 
 def human_click(point):
-	moveTime = numpy.linalg.norm(numpy.array(point) - numpy.array(pyautogui.position())) / numpy.linalg.norm(pyautogui.size())
-	pyautogui.moveTo(point[0], point[1], moveTime * numpy.random.uniform(0.4, 0.5))
-	pyautogui.mouseDown()
-	time.sleep(numpy.random.uniform(0, 0.3))
-	pyautogui.mouseUp()
-	time.sleep(numpy.random.uniform(0, 0.2))
+	moveTime = numpy.linalg.norm(numpy.array(point) - numpy.array(mouse.position)) / numpy.linalg.norm((1535, 863))
+	mouse.position = numpy.multiply(point, (1535 / 1920, 863 / 1080)) # thank you pynput
+	mouse.press(Button.left)
+	time.sleep(numpy.random.uniform(0.3, 0.5))
+	mouse.release(Button.left)
+	time.sleep(numpy.random.uniform(0.2, 0.4))
 
 def main_loop():
 	frame = ImageGrab.grab()
 	frame = numpy.array(frame)
-	print(os.listdir())
-	target = cv2.imread('0.png')
-	click_target(frame, target)
+	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	files = os.listdir()
+	files = [file for file in files if file.endswith('.png')]
+	print(files)
+	while True:
+		for file in files:
+			target = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+			tokens = file.split(' ')
+			if len(tokens) > 1 and tokens[0].lstrip('-').isdigit() and tokens[1].lstrip('-').isdigit():
+				click_target(frame, target, (int(tokens[0]), int(tokens[1])))
+			else:
+				click_target(frame, target)
 
 # kill switch
 def on_press(key):
@@ -53,18 +70,17 @@ def on_release(key):
 	print('{0} release'.format(
 	key))
 	if key == Key.esc:
+		mouse.release(Button.left)
 		# stop listener
 		return False
 
 def kill_switch():
 	# collect events until released
-	with Listener(on_press=on_press, on_release=on_release) as listener:
+	with KeyListener(on_press=on_press, on_release=on_release) as listener:
 		listener.join()
 
 # in case we run from cmd
 os.chdir(os.path.dirname(sys.argv[0]))
-
-pyautogui.FAILSAFE = False
 
 main_thread = threading.Thread(target=main_loop)
 main_thread.daemon = True # slightly cursed
