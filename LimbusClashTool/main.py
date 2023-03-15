de1c4d3568e786defd5e82b10fc7eaab67108812
null-machine <lowercase.inbox@gmail.com>
2023-03-15 14:02:36 -0400

    basic graph

--- LimbusClashTool/main.py
+++ LimbusClashTool/main.py
@@ -4,9 +4,14 @@ from skill import Skill
 
 un_deux = Skill("un_deux", "lcb_meursault", 2, 2, 4, 26)
 nailing_fist = Skill("nailing_fist", "lcb_meursault", 5, 1, 8, 26)
-des_coups = Skill("des_coups", "lcb_meursault", 4, 4, 1, 36)
+des_coups = Skill("des_coups", "lcb_meursault", 4, 4, 1, 26)
+
+to_pathos_mathos = Skill("to_pathos_mathos", "outis", 20, 1, 3, 21)
+legerdemain = Skill("legerdemain", "gregor", 18, 1, 5, 24)
 
 meursault = Sinner("lcb_meursault", un_deux, nailing_fist, des_coups)
 
 # print(yaml.dump(meursault))
-meursault.s3.gen_breakpoints(0, 0.7)
+meursault.s3.gen_breakpoints(0)
+
+# legerdemain.gen_breakpoints(0)
--- LimbusClashTool/skill.py
+++ LimbusClashTool/skill.py
@@ -1,4 +1,8 @@
+# import matplotlib
+# matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import binom
 
 class Skill:
 
@@ -10,21 +14,50 @@ class Skill:
 		self.coin_power = coin_power
 		self.offense = offense
 
-	def gen_breakpoints(self, enemy_offense, heads_chance):
-		# offense_power = (self.offense - enemy_offense) / 5
-		offense_power = 0
+	def gen_breakpoints(self, enemy_offense):
+		offense_power = (self.offense - enemy_offense) / 5
+		# offense_power = 0
 		effective_base_power = self.base_power + offense_power
-		breakpoints = {}
-		breakpoints[0] = 1
-		breakpoints[effective_base_power] = 1
-		for i in range(self.coin_count):
-			breakpoints[effective_base_power + i * self.coin_power] = heads_chance ** i
-		# breakpoints[effective_base_power + self.coin_power * self.coin_count] = heads_chance ** self.coin_count
-		x, y = zip(*breakpoints.items())
-		figure, axis = plt.subplots()
-		axis.step(x, y)
-		axis.set(ylim = (0, 1.1))
+		breakpoints = []
+		min_chance = []
+		reg_chance = []
+		max_chance = []
+		breakpoints.append(0)
+		min_chance.append(1)
+		reg_chance.append(1)
+		max_chance.append(1)
+		for i in range(self.coin_count + 1):
+			breakpoints.append(effective_base_power + i * self.coin_power)
+			# min_chance.append(0.3 ** i)
+			# reg_chance.append(0.5 ** i)
+			# max_chance.append(0.7 ** i)
+			min_chance.append(self.eval_chance(0.3, i))
+			reg_chance.append(self.eval_chance(0.5, i))
+			max_chance.append(self.eval_chance(0.7, i))
+		plt.style.use('dark_background')
+		fig, ax = plt.subplots()
+		ax.step(breakpoints, min_chance, color='magenta')
+		ax.step(breakpoints, reg_chance, color='yellow')
+		ax.step(breakpoints, max_chance, color='cyan')
+		ax.set(ylim = (0, 1.1))
+		fig.canvas.manager.set_window_title('Sinner Ultimate Scientific Analysis Model Of Generally Unknown Strategies')
+		plt.title(f'{self.user}: {self.name}')
+		plt.xlabel('power of an enemy skill with zero offense that always rolls the same')
+		plt.ylabel('chance of winning on any coin in the clash')
+		# print(binom.cdf(6, 13, 0.6))
+		# print(binom.cdf(3 - 1, 3, 0.5))
 		plt.show()
+	
+	def eval_chance(self, heads_chance, required_heads):
+		if required_heads <= 0:
+			return 1
+		chance = 0
+		coins_remaining = self.coin_count
+		while coins_remaining >= required_heads:
+			k = coins_remaining - required_heads
+			chance += (1 - chance) * (binom.cdf(k, coins_remaining, 1 - heads_chance))
+			coins_remaining -= 1
+		return chance
 	
 	def clash(enemy_skill):
 		return
