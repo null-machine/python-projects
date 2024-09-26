ff50035df2cb6c9625b5099655054fe0b70d0632
LOWERCASE <lowercase.inbox@gmail.com>
2024-09-26 09:59:04 -0400

    ytdlp shortcut

--- global.py
+++ global.py
@@ -7,11 +7,36 @@ from pynput.mouse import Controller as MouseController
 from pynput.keyboard import Key
 from pynput.keyboard import Controller as KeyboardController
 
+import ctypes
+
+CF_TEXT = 1
+
+kernel32 = ctypes.windll.kernel32
+kernel32.GlobalLock.argtypes = [ctypes.c_void_p]
+kernel32.GlobalLock.restype = ctypes.c_void_p
+kernel32.GlobalUnlock.argtypes = [ctypes.c_void_p]
+user32 = ctypes.windll.user32
+user32.GetClipboardData.restype = ctypes.c_void_p
+
+def get_clipboard_text():
+	user32.OpenClipboard(0)
+	try:
+		if user32.IsClipboardFormatAvailable(CF_TEXT):
+			data = user32.GetClipboardData(CF_TEXT)
+			data_locked = kernel32.GlobalLock(data)
+			text = ctypes.c_char_p(data_locked)
+			value = text.value
+			kernel32.GlobalUnlock(data_locked)
+			return str(value)[2:-1]
+	finally:
+		user32.CloseClipboard()
+
 ahk = AHK(executable_path="C:/Program Files/AutoHotkey/v1.1.37.01/AutoHotkeyU64.exe")
 mouse = MouseController()
 keyboard = KeyboardController()
 
 molly_mutex = False
+zipline_mutex = False
 pickaxe_mutex = False
 rbutton_state = False
 
@@ -31,6 +56,9 @@ def click_point(point):
 	frame_sleep()
 	mouse.release(Button.left)
 	small_sleep()
+
+def check_window(title):
+	return ahk.active_window and ahk.active_window.title is not None and ahk.active_window.title.startswith(title)
 
 # def kill_window():
 # 	if (ahk.active_window.process_path != 'C:\Windows\explorer.exe'):
@@ -77,14 +105,14 @@ def right_click_stack():
 # 		return
 
 def lbutton_hook():
-	if not ahk.active_window or ahk.active_window.title != 'Deep Rock Galactic  ':
+	if not check_window('Deep Rock Galactic'):
 		return
-	ahk.send("{f13 up}")
+	ahk.send("{scrolllock up}")
 
 def lbutton_up_hook():
-	if not ahk.active_window or ahk.active_window.title != 'Deep Rock Galactic  ':
+	if not check_window('Deep Rock Galactic'):
 		return
-	ahk.send('{f13 down}')
+	ahk.send('{scrolllock down}')
 	if ahk.key_state('lctrl', mode='P'):
 		ahk.send("{' down}")
 		frame_sleep()
@@ -92,7 +120,7 @@ def lbutton_up_hook():
 
 def quick_molly():
 	global molly_mutex
-	if not ahk.active_window or ahk.active_window.title != 'Deep Rock Galactic  ' or molly_mutex:
+	if not check_window('Deep Rock Galactic') or molly_mutex:
 		return
 	key_down = ahk.key_state('q', mode='P')
 	molly_mutex = True
@@ -100,26 +128,40 @@ def quick_molly():
 		ahk.send('{rbutton down}')
 		frame_sleep()
 		ahk.send('{rbutton up}')
-		# frame_sleep()
 		ahk.send('{. down}')
 		frame_sleep()
 		ahk.send('{. up}')
-		# frame_sleep()
-		key_down = ahk.key_state('y', mode='P')
+		key_down = ahk.key_state('q', mode='P')
 	molly_mutex = False
+
+def quick_zipline():
+	global zipline_mutex
+	if not check_window('Deep Rock Galactic') or zipline_mutex:
+		return
+	key_down = ahk.key_state('y', mode='P')
+	zipline_mutex = True
+	while key_down:
+		ahk.send('{space down}')
+		small_sleep()
+		ahk.send('{space up}')
+		ahk.send('{. down}')
+		frame_sleep()
+		ahk.send('{. up}')
+		key_down = ahk.key_state('y', mode='P')
+	zipline_mutex = False
 
 	# ahk.mouse_move(x=distance, y=-distance, speed=2, relative=True)
 	
 def rbutton_hook():
 	global rbutton_state
 	global pickaxe_mutex
-	if not ahk.active_window or ahk.active_window.title != 'Deep Rock Galactic  ':
+	rbutton_state = True
+	if not check_window('Deep Rock Galactic'):
 		return
-	ahk.send('{f13 up}')
+	ahk.send('{scrolllock up}')
 	if pickaxe_mutex:
 		return
 	pickaxe_mutex = True
-	rbutton_state = True
 	while rbutton_state:
 		ahk.send('{rbutton down}')
 		time.sleep(0.6)
@@ -130,102 +172,183 @@ def rbutton_hook():
 def rbutton_up_hook():
 	global rbutton_state
 	rbutton_state = False
-	ahk.send('{f13 down}')
+	if not check_window('Deep Rock Galactic'):
+		return
+	ahk.send('{scrolllock down}')
 
-# dice_position = mouse.position
-# card_position = mouse.position
+dice_position = mouse.position
+card_position = mouse.position
 
-# def save_dice_position():
-# 	global dice_position
-# 	dice_position = mouse.position
-# 	mouse.press(Button.right)
-# 	frame_sleep()
-# 	mouse.release(Button.right)
-# 	frame_sleep()
-# 	mouse.press(Button.left)
-# 	frame_sleep()
-# 	mouse.release(Button.left)
-# 	frame_sleep()
+def save_dice_position():
+	global dice_position
+	if not check_window('LibraryOfRuina'):
+		return
+	dice_position = mouse.position
+	frame_sleep()
+	mouse.press(Button.right)
+	frame_sleep()
+	mouse.release(Button.right)
+	frame_sleep()
+	mouse.press(Button.left)
+	frame_sleep()
+	mouse.release(Button.left)
+	frame_sleep()
+	return
+
+def save_card_position():
+	global card_position
+	if not check_window('LibraryOfRuina'):
+		return
+	card_position = mouse.position
+	frame_sleep()
+	mouse.press(Button.left)
+	frame_sleep()
+	mouse.release(Button.left)
+	frame_sleep()
+	return
+
+def retarget_mass():
+	if not check_window('LibraryOfRuina'):
+		return
+	target_position = mouse.position
+	mouse.position = dice_position
+	frame_sleep()
+	save_dice_position()
+	mouse.position = card_position
+	frame_sleep()
+	save_card_position()
+	mouse.position = target_position
+	frame_sleep()
+	mouse.press(Button.left)
+	frame_sleep()
+	mouse.release(Button.left)
+	frame_sleep()
+	mouse.move(0, -64)
+	frame_sleep()
+	mouse.move(0, 64)
+	frame_sleep()
+	return
+
+def quick_on_play():
+	if not check_window('LibraryOfRuina'):
+		return
+	target_position = mouse.position
+	mouse.press(Button.left)
+	frame_sleep()
+	mouse.release(Button.left)
+	frame_sleep()
+	mouse.position = dice_position
+	frame_sleep()
+	mouse.press(Button.left)
+	frame_sleep()
+	mouse.release(Button.left)
+	frame_sleep()
+	mouse.position = target_position
+	frame_sleep()
+	return
+
+watching_pixel = False
+
+def toggle_pixel_watch():
+	global watching_pixel
+	if watching_pixel:
+		stop_pixel_watch()
+	else:
+		start_pixel_watch()
+
+def start_pixel_watch():
+	global watching_pixel
+	watching_pixel = True
+	while (watching_pixel):
+		# print(ahk.pixel_get_color(960, 660))
+		if ahk.pixel_get_color(960, 651) == '0xFE1D5C':
+			mouse.press(Button.left)
+			frame_sleep()
+			mouse.release(Button.left)
+			small_sleep()
+
+def stop_pixel_watch():
+	global watching_pixel
+	watching_pixel = False
+
+def ytdlp():
+	ahk.run_script(f'run \"C:\\Home\\ytdlp\\yt-dlp.exe" {get_clipboard_text()}')
+
+# def wheel_pgup():
+# 	if not check_window('Terraria'):
+# 		return
+# 	ahk.send('{pgup down}')
+# 	# frame_sleep()
+# 	ahk.send('{pgup up}')
 # 	return
 
-# def save_card_position():
-# 	global card_position
-# 	card_position = mouse.position
-# 	mouse.press(Button.left)
-# 	frame_sleep()
-# 	mouse.release(Button.left)
-# 	frame_sleep()
-# 	return
-
-# def retarget_mass():
-# 	target_position = mouse.position
-# 	mouse.position = dice_position
-# 	frame_sleep()
-# 	save_dice_position()
-# 	mouse.position = card_position
-# 	frame_sleep()
-# 	save_card_position()
-# 	mouse.position = target_position
-# 	frame_sleep()
-# 	mouse.press(Button.left)
-# 	frame_sleep()
-# 	mouse.release(Button.left)
-# 	frame_sleep()
-# 	mouse.move(0, -64)
-# 	frame_sleep()
-# 	mouse.move(0, 64)
-# 	frame_sleep()
+# def wheel_pgdn():
+# 	if not check_window('Terraria'):
+# 		return
+# 	ahk.send('{pgdn down}')
+# 	# frame_sleep()
+# 	ahk.send('{pgdn up}')
 # 	return
 
 # system commands
 # ahk.add_hotkey('!del', callback=kill_window)
 # ahk.add_hotkey('!\'', callback=open_logbook)
-ahk.add_hotkey('!`', callback=open_logbook)
-ahk.add_hotkey('pause', callback=lambda: ahk.send('{esc}')) # null symbol
+ahk.add_hotkey('$!`', callback=open_logbook)
+ahk.add_hotkey('$!insert', callback=ytdlp)
+ahk.add_hotkey('$pause', callback=lambda: ahk.send('{esc}'))
 
 # unicode mappings
-ahk.add_hotkey('!^space', callback=lambda: ahk.send('{u+202e}')) # rtl override
-ahk.add_hotkey('!^+space', callback=lambda: ahk.send('{u+206b}')) # rtl embedding
+ahk.add_hotkey('$!^space', callback=lambda: ahk.send('{u+202e}')) # rtl override
+ahk.add_hotkey('$!^+space', callback=lambda: ahk.send('{u+206b}')) # rtl embedding
+ahk.add_hotkey('$!8', callback=lambda: ahk.send('{u+2730}')) # star
 # ahk.add_hotkey('space', callback=lambda: ahk.send('{u+200a}')) # thin space
 # ahk.add_hotkey('space', callback=lambda: ahk.send('{u+2002}')) # thin space
-ahk.add_hotkey('!;', callback=lambda: ahk.send('{u+2014}')) # emdash
-ahk.add_hotkey('![', callback=lambda: ahk.send('{u+2190}')) # arrow left
-ahk.add_hotkey('!]', callback=lambda: ahk.send('{u+2192}')) # arrow right
-ahk.add_hotkey('!/', callback=lambda: ahk.send('{u+00f7}')) # division symbol
+# ahk.add_hotkey('!;', callback=lambda: ahk.send('{u+2014}')) # emdash
+ahk.add_hotkey('$![', callback=lambda: ahk.send('{u+2190}')) # arrow left
+ahk.add_hotkey('$!]', callback=lambda: ahk.send('{u+2192}')) # arrow right
+ahk.add_hotkey('$!/', callback=lambda: ahk.send('{u+00f7}')) # division symbol
 # ahk.add_hotkey('!0', callback=lambda: ahk.send('{u+00d8}')) # null symbol
-ahk.add_hotkey('!s', callback=lambda: ahk.send('{u+00a7}')) # section sign
-ahk.add_hotkey('!0', callback=lambda: ahk.send('{u+25ae}')) # cuniform+
+ahk.add_hotkey('$!s', callback=lambda: ahk.send('{u+00a7}')) # section sign
+ahk.add_hotkey('$!0', callback=lambda: ahk.send('{u+25ae}')) # cuniform+
 # 00bf inverted qmark
 
 # game inputs
-ahk.add_hotkey('!o', callback=lambda: keyboard.press(','))
-ahk.add_hotkey('ralt & lbutton', callback=lambda: ahk.send('{lbutton down}'))
-ahk.add_hotkey('ralt & rbutton', callback=lambda: ahk.send('{rbutton down}'))
-ahk.add_hotkey('!wheeldown', callback=click_stack)
-ahk.add_hotkey('!wheelup', callback=right_click_stack)
+ahk.add_hotkey('$!o', callback=lambda: keyboard.press(','))
+ahk.add_hotkey('rctrl & lbutton', callback=lambda: ahk.send('{lbutton down}'))
+ahk.add_hotkey('rctrl & rbutton', callback=lambda: ahk.send('{rbutton down}'))
+ahk.add_hotkey('$!,', callback=lambda: ahk.send('{, down}'))
+ahk.add_hotkey('$!wheeldown', callback=click_stack)
+ahk.add_hotkey('$!wheelup', callback=right_click_stack)
+
+# ahk.add_hotkey('$scrolllock', callback=toggle_pixel_watch)
 # ahk.add_hotkey('q', callback=emote_zaw)
 # ahk.add_hotkey('q up', callback=emote_zaw_cleanup)
-ahk.add_hotkey('~*lbutton', callback=lbutton_hook)
-ahk.add_hotkey('~*lbutton up', callback=lbutton_up_hook)
-ahk.add_hotkey('~*$q', callback=quick_molly)
-# ahk.add_hotkey('~*$`', callback=tcf_self)
-ahk.add_hotkey('~*$rbutton', callback=rbutton_hook)
-ahk.add_hotkey('~*$rbutton up', callback=rbutton_up_hook)
-# ahk.add_hotkey('~*q', callback=spiral_flame)
 
-# ahk.add_hotkey('f8 up', callback=save_dice_position)
-# ahk.add_hotkey('f7 up', callback=save_card_position)
-# ahk.add_hotkey('f6 up', callback=retarget_mass)
+# ahk.add_hotkey('$wheelup', callback=wheel_pgup)
+# ahk.add_hotkey('$wheeldown', callback=wheel_pgdn)
+
+# ahk.add_hotkey('$~*lbutton', callback=lbutton_hook)
+# ahk.add_hotkey('$~*lbutton up', callback=lbutton_up_hook)
+# ahk.add_hotkey('$~*q', callback=quick_molly)
+# ahk.add_hotkey('$~*y', callback=quick_zipline)
+# ahk.add_hotkey('$~*rbutton', callback=rbutton_hook)
+# ahk.add_hotkey('$~*rbutton up', callback=rbutton_up_hook)
+# ahk.add_hotkey('$~*backspace', callback=lalt_hook)
+
+ahk.add_hotkey('f8 up', callback=save_dice_position)
+ahk.add_hotkey('f7 up', callback=save_card_position)
+ahk.add_hotkey('f6 up', callback=retarget_mass)
+ahk.add_hotkey('f5 up', callback=quick_on_play)
 
 # media controls
 # ahk.add_hotkey('~mbutton & wheeldown', callback=lambda: ahk.send('{volume_down}'))
 # ahk.add_hotkey('~mbutton & wheelup', callback=lambda: ahk.send('{volume_up}'))
 # ahk.add_hotkey('~mbutton & lbutton', callback=lambda: ahk.send('{media_play_pause}'))
 # ahk.add_hotkey('~mbutton & rbutton', callback=lambda: ahk.send('{media_next}'))
-ahk.add_hotkey('ralt & wheeldown', callback=lambda: ahk.send('{volume_down}'))
-ahk.add_hotkey('ralt & wheelup', callback=lambda: ahk.send('{volume_up}'))
-ahk.add_hotkey('!f2 up', callback=lambda: ahk.send('{media_play_pause}'))
-ahk.add_hotkey('!f3 up', callback=lambda: ahk.send('{media_next}'))
+ahk.add_hotkey('$!;', callback=lambda: ahk.send('{u+2014}'))
+# ahk.add_hotkey('$!q', callback=lambda: ahk.send('{volume_up}'))
+ahk.add_hotkey('$!f2 up', callback=lambda: ahk.send('{media_play_pause}'))
+ahk.add_hotkey('$!f3 up', callback=lambda: ahk.send('{media_next}'))
 
 # ahk.add_hotkey('ralt & wheeldown', callback=lambda: ahk.send('{volume_down}'))
 # ahk.add_hotkey('ralt & wheelup', callback=lambda: ahk.send('{volume_up}'))
@@ -239,3 +362,5 @@ ahk.block_forever()
 # isolate: u+2067
 # override: u+202E
 # arabic mark: u+061C
+
+print("test")
---
+++ vlc-delete.lua
@@ -0,0 +1,145 @@
+--[[
+	Copyright 2015-2023 surrim
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+]]--
+
+function descriptor()
+	return {
+		title = "VLC Delete";
+		version = "0.1";
+		author = "surrim";
+		url = "https://github.com/surrim/vlc-delete/";
+		shortdesc = "&Remove current file from playlist and disk";
+		description = [[
+<h1>vlc-delete</h1>"
+When you're playing a file, use VLC Delete to
+delete the current file from your playlist and <b>disk</b> with one click.<br />
+This extension has been tested on GNU Linux with VLC 2.x and 3.x.<br />
+The author is not responsible for damage caused by this extension.
+		]];
+	}
+end
+
+function file_exists(name)
+	local f=io.open(name,"r")
+	if f~=nil then io.close(f) return true else return false end
+end
+
+function sleep(seconds)
+	local t_0 = os.clock()
+	while os.clock() - t_0 <= seconds do end
+end
+
+function windows_delete(file, trys, pause)
+	if os.remove(file) then
+		return true
+	end
+	if not file_exists(file) then
+		return nil, "File does not exist"
+	end
+	for i = trys, 1, -1
+	do
+		os.execute("del /q \"" .. file .. "\"")
+		if not file_exists(file) then
+			return true
+		end
+		sleep(pause)
+	end
+	return nil, "Unable to delete file"
+end
+
+function remove_from_playlist_and_hdd()
+	local id = vlc.playlist.current()
+	vlc.playlist.next()
+	sleep(1)
+	vlc.playlist.delete(id)
+end
+
+function command_exists(command)
+	retval, err = os.execute(command)
+	return retval ~= nil
+end
+
+function current_uri_and_os()
+	local item = (vlc.player or vlc.input).item()
+	local uri = item:uri()
+	local is_posix = (package.config:sub(1, 1) == "/")
+	if uri:find("^file:///") ~= nil then
+		uri = string.gsub(uri, "^file:///", "")
+		uri = vlc.strings.decode_uri(uri)
+		if is_posix then
+			uri = "/" .. uri
+		else
+			uri = string.gsub(uri, "/", "\\")
+		end
+	end
+	return uri, is_posix
+end
+
+function activate()
+	local uri, is_posix = current_uri_and_os()
+	if is_posix then
+		local trash_put_exists = command_exists("trash-put --version > /dev/null")
+		local rm_exists = command_exists("rm --version > /dev/null")
+
+		if trash_put_exists then
+			vlc.msg.info("[vlc-delete] removing: " .. uri .. " (using trash-put)")
+			uri = string.gsub(uri, "\"", "\\\"")
+			retval, err = os.execute("trash-put \"" .. uri .. "\"")
+		elseif rm_exists then
+			vlc.msg.info("[vlc-delete] removing: " .. uri .. " (using rm)")
+			uri = string.gsub(uri, "\"", "\\\"")
+			retval, err = os.execute("rm \"" .. uri .. "\"")
+		else
+			vlc.msg.info("[vlc-delete] removing: " .. uri .. " (using os.remove)")
+			retval, err = os.remove(file)
+		end
+
+		if retval ~= nil then
+			remove_from_playlist_and_hdd()
+		end
+	else
+		vlc.msg.info("[vlc-delete] removing: " .. uri)
+		remove_from_playlist_and_hdd() -- remove first so the file isn't locked by VLC
+		retval, err = windows_delete(uri, 3, 1)
+	end
+
+	if retval == nil then
+		vlc.msg.info("[vlc-delete] error: " .. (err or "nil"))
+		d = vlc.dialog("VLC Delete")
+		d:add_label("Could not remove \"" .. uri .. "\"", 1, 1, 1, 1)
+		d:add_label(err, 1, 2, 1, 1)
+		d:add_button("OK", click_ok, 1, 3, 1, 1)
+		d:show()
+	else
+		deactivate()
+	end
+end
+
+function click_ok()
+	d:delete()
+	deactivate()
+end
+
+function deactivate()
+	vlc.deactivate()
+end
+
+function close()
+	deactivate()
+end
+
+function meta_changed()
+end
